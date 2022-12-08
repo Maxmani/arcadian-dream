3d56f9ce0bf9d8b1ad88d0ef04378075976b9277
Maxmani <mikael.hannukainen@gmail.com>
2022-12-08 04:14:35 +0200

    Add Onbashiras to EMI's "world interaction" category

--- src/main/java/net/reimaden/arcadiandream/compat/emi/EMIPlugin.java
+++ src/main/java/net/reimaden/arcadiandream/compat/emi/EMIPlugin.java
@@ -3,11 +3,15 @@ package net.reimaden.arcadiandream.compat.emi;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.recipe.EmiWorldInteractionRecipe;
+import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
+import net.minecraft.item.Items;
 import net.minecraft.recipe.RecipeManager;
 import net.minecraft.util.Identifier;
 import net.reimaden.arcadiandream.ArcadianDream;
 import net.reimaden.arcadiandream.block.ModBlocks;
+import net.reimaden.arcadiandream.item.ModItems;
 import net.reimaden.arcadiandream.recipe.RitualCraftingRecipe;
 
 public class EMIPlugin implements EmiPlugin {
@@ -26,5 +30,17 @@ public class EMIPlugin implements EmiPlugin {
         for (RitualCraftingRecipe recipe : rm.listAllOfType(RitualCraftingRecipe.Type.INSTANCE)) {
             registry.addRecipe(new EMIRitualCraftingRecipe(recipe));
         }
+
+        // Add modded items & blocks to some of EMI's categories
+        EmiIngredient lead = EmiStack.of(Items.LEAD);
+        EmiIngredient paper = EmiStack.of(Items.PAPER);
+
+        registry.addRecipe(EmiWorldInteractionRecipe.builder()
+                .id(new Identifier(ArcadianDream.MOD_ID, "/onbashira_crafting"))
+                .leftInput(EmiStack.of(ModItems.ONBASHIRA_PILLAR))
+                .rightInput(lead, false)
+                .rightInput(paper, false)
+                .output(EmiStack.of(ModBlocks.ONBASHIRA))
+                .build());
     }
 }
