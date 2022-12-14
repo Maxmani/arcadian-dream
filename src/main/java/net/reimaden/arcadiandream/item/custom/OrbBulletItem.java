5171e98c4f78865a1791ecc0573bb9a814fa8525
Maxmani <mikael.hannukainen@gmail.com>
2022-12-14 22:02:39 +0200

    Fix ring shots (thanks Foxigami)

--- src/main/java/net/reimaden/arcadiandream/item/custom/OrbBulletItem.java
+++ src/main/java/net/reimaden/arcadiandream/item/custom/OrbBulletItem.java
@@ -1,5 +1,6 @@
 package net.reimaden.arcadiandream.item.custom;
 
+import net.minecraft.util.math.Vec3d;
 import net.reimaden.arcadiandream.entity.custom.OrbBulletEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
@@ -8,7 +9,7 @@ import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
-@SuppressWarnings("ConstantConditions")
+@SuppressWarnings({"ConstantConditions", "ParameterCanBeLocal"})
 public class OrbBulletItem extends BaseBulletItem {
 
     public OrbBulletItem(Settings settings) {
@@ -19,6 +20,16 @@ public class OrbBulletItem extends BaseBulletItem {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
 
+        int d = itemStack.getNbt().getInt("Density");
+        int k = itemStack.getNbt().getInt("Stack");
+        int m = itemStack.getNbt().getInt("Modifier");
+        float s = itemStack.getNbt().getFloat("Speed");
+        float angle = 0;
+        float x = 0;
+        float y = 0;
+        float speed = 0;
+        float n = s / k;
+
         if (!world.isClient) {
             if (itemStack.getNbt().getString("Pattern").equals("None")) {
                 for (int i = 0; i < itemStack.getNbt().getInt("Density"); i++) {
@@ -28,46 +39,42 @@ public class OrbBulletItem extends BaseBulletItem {
                     world.spawnEntity(orbBulletEntity);
                 }
             } else if (itemStack.getNbt().getString("Pattern").equals("Ring")) {
-                float d = itemStack.getNbt().getInt("Density");
-                float L = user.getPitch();
-                for (int i = 0; i < d; i++) {
-                    OrbBulletEntity orbBulletEntity = new OrbBulletEntity(world, user);
-                    orbBulletEntity.setItem(itemStack);
-                    orbBulletEntity.setVelocity(user, L * MathHelper.cos(i * (360 / d)),
-                            user.getYaw() + (i * (360 / d)), 0.0f, itemStack.getNbt().getFloat("Speed"), 0.0f);
-                    world.spawnEntity(orbBulletEntity);
-                }
-            } else if (itemStack.getNbt().getString("Pattern").equals("Ray")) {
-                int x = itemStack.getNbt().getInt("Density");
-                float j = itemStack.getNbt().getFloat("Speed");
-                float k = itemStack.getNbt().getFloat("Speed") / x;
-                for (int i = 0; i < itemStack.getNbt().getInt("Density"); i++) {
-                    OrbBulletEntity orbBulletEntity = new OrbBulletEntity(world, user);
-                    orbBulletEntity.setItem(itemStack);
-                    orbBulletEntity.setVelocity(user, user.getPitch(), user.getYaw(), 0.0f, j, 0.0f);
-                    world.spawnEntity(orbBulletEntity);
-                    j = j - k;
-                }
-            }
-            else if (itemStack.getNbt().getString("Pattern").equals("Stack Ring")) {
-                int d = itemStack.getNbt().getInt("Density");
-                int e = itemStack.getNbt().getInt("Stack");
-                float j = itemStack.getNbt().getFloat("Speed");
-                float k = itemStack.getNbt().getFloat("Speed") / e;
-                for (int f = 0; f < e; f++) {
-                    for (int i = 0; i < d; i++) {
-                        OrbBulletEntity orbBulletEntity = new OrbBulletEntity(world, user);
-                        orbBulletEntity.setItem(itemStack);
-                        orbBulletEntity.setVelocity(user, 0.0f,
-                                user.getYaw() + (i * (360 / (float) d)), 0.0f, j, 0.0f);
-                        world.spawnEntity(orbBulletEntity);
-                    }
-                    j = j - k;
-                }
+                createRing(world, user, itemStack, d, k, angle, s, speed, m, x, y, n);
             }
         }
 
         return super.use(world, user, hand);
+    }
+
+    private void createRing(World world, PlayerEntity user, ItemStack itemStack,
+                            int d, int k, float angle, float s, float speed, int m, float x, float y, float n) {
+        for (int i = 0; i < k; i++) {
+            for (int h = 0; h < d; h++) {
+                OrbBulletEntity orbBulletEntity = new OrbBulletEntity(world, user);
+                orbBulletEntity.setItem(itemStack);
+
+                angle = (h * (360 / (float)d)) + 180;
+                speed = s;
+                if (m != 0) {
+                    m = 1 + m;
+                    x = MathHelper.cos(angle);
+                    y = MathHelper.sin(angle) / 2; //replace 2 with m when modifier is correctly programmed in.
+                    angle = (float)MathHelper.atan2(y, x) * MathHelper.DEGREES_PER_RADIAN;
+                    speed = MathHelper.sqrt(MathHelper.square(x) + MathHelper.square(y)) * s;
+                }
+                Vec3d bullet = new Vec3d(0, 0, 1);
+                bullet = bullet.rotateY((angle) * MathHelper.RADIANS_PER_DEGREE);
+                bullet = bullet.rotateX(user.getPitch() * MathHelper.RADIANS_PER_DEGREE);
+                bullet = bullet.rotateY((-user.getYaw() + 180) * MathHelper.RADIANS_PER_DEGREE);
+
+                orbBulletEntity.setVelocity(bullet.getX(),
+                        bullet.getY(), bullet.getZ(), speed, 0.0f);
+
+                world.spawnEntity(orbBulletEntity);
+            }
+
+            s = s - n;
+        }
     }
 
     @Override
