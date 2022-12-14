0c55a06bb2ab2b0ac7d891d60e0abc52cce3592e
Maxmani <mikael.hannukainen@gmail.com>
2022-12-14 16:18:38 +0200

    Fix things

--- src/main/java/net/reimaden/arcadiandream/block/entity/RitualShrineBlockEntity.java
+++ src/main/java/net/reimaden/arcadiandream/block/entity/RitualShrineBlockEntity.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
 import net.fabricmc.fabric.api.networking.v1.PlayerLookup;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.stat.Stats;
 import net.reimaden.arcadiandream.block.custom.RitualShrineBlock;
 import net.reimaden.arcadiandream.networking.ModMessages;
 import net.reimaden.arcadiandream.recipe.RitualCraftingRecipe;
@@ -127,6 +128,7 @@ public class RitualShrineBlockEntity extends BlockEntity implements ImplementedI
             shrineBlock.markDirty();
             craftEffects();
             player.incrementStat(ModStats.INTERACT_WITH_RITUAL_SHRINE);
+            player.incrementStat(Stats.CRAFTED.getOrCreateStat(recipe.get().getOutput().getItem()));
         }
     }
 
--- src/main/java/net/reimaden/arcadiandream/item/custom/MochiHammerItem.java
+++ src/main/java/net/reimaden/arcadiandream/item/custom/MochiHammerItem.java
@@ -19,7 +19,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-
 public class MochiHammerItem extends ModHammerItem {
 
     private final float attackDamage;
