0dd6c52b28ecc87bebbdcc19e001f591e2ddb5c0
Maxmani <mikael.hannukainen@gmail.com>
2023-03-21 01:51:08 +0200

    Make pattern templates obtainable

--- src/main/java/net/reimaden/arcadiandream/util/ModLootTableModifiers.java
+++ src/main/java/net/reimaden/arcadiandream/util/ModLootTableModifiers.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Maxmani and contributors.
+ * Copyright (c) 2022-2023 Maxmani and contributors.
  * Licensed under the EUPL-1.2 or later.
  */
 
@@ -28,7 +28,9 @@ import java.util.Map;
 
 public class ModLootTableModifiers {
 
-    private static final int peach_height = ArcadianDream.CONFIG.hisouSwordOptions.minHeightForPeaches();
+    private static final int PEACH_HEIGHT = ArcadianDream.CONFIG.hisouSwordOptions.minHeightForPeaches();
+    private static final float TEMPLATE_CHANCE = 0.25f;
+    private static final float TEMPLATE_CHANCE_ALT = 0.10f;
 
     public static void modify() {
         LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
@@ -51,9 +53,81 @@ public class ModLootTableModifiers {
                             .conditionally(RandomChanceLootCondition.builder(0.10f))
                             .with(ItemEntry.builder(ModItems.HEAVENLY_PEACH))
                             .conditionally(MatchToolLootCondition.builder(ItemPredicate.Builder.create().items(ModItems.HISOU_SWORD)))
-                            .conditionally(LocationCheckLootCondition.builder(LocationPredicate.Builder.create().y(NumberRange.FloatRange.atLeast(peach_height))));
+                            .conditionally(LocationCheckLootCondition.builder(LocationPredicate.Builder.create().y(NumberRange.FloatRange.atLeast(PEACH_HEIGHT))));
                     tableBuilder.pool(poolBuilder.build());
                 }
+            }
+
+            // Pattern templates
+            if (LootTables.SHIPWRECK_SUPPLY_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE_ALT))
+                        .with(ItemEntry.builder(ModItems.SPREAD_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.SHIPWRECK_TREASURE_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE_ALT))
+                        .with(ItemEntry.builder(ModItems.SPREAD_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.SHIPWRECK_MAP_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE_ALT))
+                        .with(ItemEntry.builder(ModItems.SPREAD_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.PILLAGER_OUTPOST_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE))
+                        .with(ItemEntry.builder(ModItems.RAY_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.NETHER_BRIDGE_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE))
+                        .with(ItemEntry.builder(ModItems.RING_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.ANCIENT_CITY_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE))
+                        .with(ItemEntry.builder(ModItems.CONE_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.ABANDONED_MINESHAFT_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE))
+                        .with(ItemEntry.builder(ModItems.DOUBLE_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.STRONGHOLD_CORRIDOR_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE_ALT))
+                        .with(ItemEntry.builder(ModItems.TRIPLE_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.STRONGHOLD_CROSSING_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE_ALT))
+                        .with(ItemEntry.builder(ModItems.TRIPLE_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
+            }
+            if (LootTables.STRONGHOLD_LIBRARY_CHEST.equals(id)) {
+                poolBuilder
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(TEMPLATE_CHANCE_ALT))
+                        .with(ItemEntry.builder(ModItems.TRIPLE_PATTERN_TEMPLATE));
+                tableBuilder.pool(poolBuilder.build());
             }
         });
     }
