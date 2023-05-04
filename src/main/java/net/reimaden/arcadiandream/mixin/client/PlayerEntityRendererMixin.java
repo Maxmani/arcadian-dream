d58e8ab06e517c82931ae80e0afb7c4ccf05cca7
Maxmani <mikael.hannukainen@gmail.com>
2023-05-05 00:36:25 +0300

    Fix compatibility with Origins (very dirty)

--- src/main/java/net/reimaden/arcadiandream/mixin/client/PlayerEntityRendererMixin.java
+++ src/main/java/net/reimaden/arcadiandream/mixin/client/PlayerEntityRendererMixin.java
@@ -7,38 +7,27 @@ package net.reimaden.arcadiandream.mixin.client;
 
 import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
-import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.reimaden.arcadiandream.effect.ModEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntityRenderer.class)
 public class PlayerEntityRendererMixin {
 
-    @Redirect(method = "renderArm", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V", ordinal = 0))
-    private void arcadiandream$makeArmTranslucent(ModelPart modelPart, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, MatrixStack matrices2, VertexConsumerProvider vertexConsumers, int light2, AbstractClientPlayerEntity player) {
+    @Inject(method = "renderArm", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V", ordinal = 0), cancellable = true)
+    private void arcadiandream$makeArmTranslucent(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, AbstractClientPlayerEntity player, ModelPart arm, ModelPart sleeve, CallbackInfo ci) {
         if (player.hasStatusEffect(ModEffects.ETHEREAL)) {
             final float alpha = 0.75f;
-            modelPart.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(player.getSkinTexture())), light, overlay, 1.0f, 1.0f, 1.0f, alpha);
-            return;
+            arm.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(player.getSkinTexture())), light, OverlayTexture.DEFAULT_UV, 1.0f, 1.0f, 1.0f, alpha);
+            sleeve.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(player.getSkinTexture())), light, OverlayTexture.DEFAULT_UV, 1.0f, 1.0f, 1.0f, alpha);
+            ci.cancel();
         }
-
-        modelPart.render(matrices, vertices, light, overlay);
-    }
-
-    @Redirect(method = "renderArm", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;II)V", ordinal = 1))
-    private void arcadiandream$makeSleeveTranslucent(ModelPart modelPart, MatrixStack matrices, VertexConsumer vertices, int light, int overlay, MatrixStack matrices2, VertexConsumerProvider vertexConsumers, int light2, AbstractClientPlayerEntity player) {
-        if (player.hasStatusEffect(ModEffects.ETHEREAL)) {
-            final float alpha = 0.75f;
-            modelPart.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(player.getSkinTexture())), light, overlay, 1.0f, 1.0f, 1.0f, alpha);
-            return;
-        }
-
-        modelPart.render(matrices, vertices, light, overlay);
     }
 }
