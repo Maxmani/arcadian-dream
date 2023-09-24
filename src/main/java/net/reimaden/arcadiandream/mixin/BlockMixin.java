14d8d2443631387887b37642dfb897d6c8384e51
Maxmani <mikael.hannukainen@gmail.com>
2023-09-24 23:57:08 +0300

    Give Hihi'irokane tools auto-smelt

---
+++ src/main/java/net/reimaden/arcadiandream/mixin/BlockMixin.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2023 Maxmani and contributors.
+ * Licensed under the EUPL-1.2 or later.
+ */
+
+package net.reimaden.arcadiandream.mixin;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.inventory.SimpleInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ToolItem;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.recipe.SmeltingRecipe;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.reimaden.arcadiandream.item.ModToolMaterials;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/* Credits to UltrusBot for the original code
+ * https://github.com/UltrusBot/AutosmeltEnchant/blob/1.19.4/src/main/java/me/ultrusmods/autosmeltenchant/mixin/BlockMixin.java
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2022 Ultrus
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+@Mixin(Block.class)
+public class BlockMixin {
+
+    @Inject(method = "getDroppedStacks(Lnet/minecraft/block/BlockState;Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/entity/Entity;Lnet/minecraft/item/ItemStack;)Ljava/util/List;", at = @At("RETURN"), cancellable = true)
+    private static void arcadiandream$getDroppedStacks(BlockState state, ServerWorld world, BlockPos pos, BlockEntity blockEntity, Entity entity, ItemStack stack, CallbackInfoReturnable<List<ItemStack>> cir) {
+        List<ItemStack> items = new ArrayList<>();
+        List<ItemStack> returnValue = cir.getReturnValue();
+
+        // TODO: Make auto-smelting optional
+        if (((ToolItem) stack.getItem()).getMaterial() != ModToolMaterials.HIHIIROKANE) {
+            cir.setReturnValue(returnValue);
+            return;
+        }
+
+        for (ItemStack itemStack : returnValue) {
+            Optional<SmeltingRecipe> recipe = world.getRecipeManager().getFirstMatch(RecipeType.SMELTING, new SimpleInventory(itemStack), world);
+
+            if (recipe.isPresent()) {
+                ItemStack smelted = recipe.get().getOutput(world.getRegistryManager()).copy();
+                smelted.setCount(itemStack.getCount());
+                items.add(smelted);
+            } else {
+                items.add(itemStack);
+            }
+        }
+
+        cir.setReturnValue(items);
+    }
+}
